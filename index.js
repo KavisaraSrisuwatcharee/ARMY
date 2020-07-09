8b5b834a7eded659d36d78a6697501e1ee1f7c0f
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-07-09 23:29:38 +0700

    [ADD] test server

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ app.get('/api/test', (req, res) => {
     res.send({ msg: 'hello server' });
 });
 
+app.get('/', (req, res) => {
+    res.send({ msg: 'Welcome to BTS album' });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
