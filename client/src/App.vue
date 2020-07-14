4a519450c71bc8805c135a159e3baa98efef1e4a
Chingly <kavisara.noya@mail.kmutt.ac.th>
2020-07-14 21:13:13 +0700

    [ADD] Navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,50 +1,16 @@
 <template>
-    <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link>
-        </div>
-        <router-view />
-    </div>
+ <div>
+   <router-view/>
+ </div>
 </template>
-<script>
-import axios from 'axios';
-export default {
-    async mounted() {
-        let chk = true;
-        if (!chk) return;
-        console.log('inside mounted');
-        const res = await axios.get('/api/test');
-        console.log(res.data);
 
-        const albums = await axios.get('/api/albumList');
-        const id = albums.data[1]._id;
-        const body = { id, number: 12 };
-        // const r = await axios.post('/api/buy/album', body);
-        const r = await axios.post('/api/sell/album', body);
-        console.log(r.data);
-    },
+<script>
+
+export default {
+ 
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
-
-#nav {
-    padding: 30px;
-}
-
-#nav a {
-    font-weight: bold;
-    color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-    color: #42b983;
-}
-</style>
