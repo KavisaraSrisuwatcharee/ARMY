4be07855f6c824758938c7953985951d4392c94c
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-07-15 15:11:51 +0700

    [FIX] conflict

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,27 +7,10 @@
 <script>
 
 export default {
-<<<<<<< HEAD
-    async mounted() {
-        let chk = false;
-        if (!chk) return;
-        console.log('inside mounted');
-        const res = await axios.get('/api/test');
-        console.log(res.data);
-
-        const albums = await axios.get('/api/albumlist');
-        const id = albums.data[1]._id;
-        const body = { id, number: 12 };
-        const r = await axios.post('/api/buy/album', body);
-        // const r = await axios.post('/api/sell/album', body);
-        console.log(r.data);
-    },
-=======
  
 
   data: () => ({
     //
   }),
->>>>>>> 4a519450c71bc8805c135a159e3baa98efef1e4a
 };
 </script>
